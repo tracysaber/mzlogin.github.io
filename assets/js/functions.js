af4ca521c699232ec73232f381f2f756d5fe46a0
tracysaber <402807046@qq.com>
2017-12-20 10:20:46 +0800

    love

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,12 +6,12 @@ var clientHeight = $(window).height();
 $(function () {
     // setup garden
 	$loveHeart = $("#loveHeart");
-	var offsetX = $loveHeart.width() / 2;
-	var offsetY = $loveHeart.height() / 2 - 55;
+	var offsetX = $loveHeart.width() / 2 +200;
+	var offsetY = $loveHeart.height() / 2 - 55 +200;
     $garden = $("#garden");
     gardenCanvas = $garden[0];
 	gardenCanvas.width = $("#loveHeart").width();
-    gardenCanvas.height = $("#loveHeart").height()
+    gardenCanvas.height = $("#loveHeart").height();
     gardenCtx = gardenCanvas.getContext("2d");
     gardenCtx.globalCompositeOperation = "lighter";
     garden = new Garden(gardenCtx, gardenCanvas);
